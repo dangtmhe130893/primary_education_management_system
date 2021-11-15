8a8c15c195e124875cf15bf553e5f249314ef98a
dangtm <dangtmhe130893@fpt.edu.vn>
2021-11-15 21:41:59 +0700

    update header

--- src/main/java/com/primary_education_system/config/security/CustomUserDetails.java
+++ src/main/java/com/primary_education_system/config/security/CustomUserDetails.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 
@@ -27,5 +28,10 @@ public class CustomUserDetails extends User {
     public CustomUserDetails(String username, String password, Long userId, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, authorities);
         this.userId = userId;
+        this.year = Calendar.getInstance().get(Calendar.YEAR);
+        this.month = Calendar.getInstance().get(Calendar.MONTH) + 1;
+        this.day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        this.hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        this.minutes = Calendar.getInstance().get(Calendar.MINUTE);
     }
 }
--- src/main/java/com/primary_education_system/controller/UserInfoHeaderController.java
+++ src/main/java/com/primary_education_system/controller/UserInfoHeaderController.java
@@ -1,7 +1,7 @@
 package com.primary_education_system.controller;
 
 import com.primary_education_system.config.security.CustomUserDetails;
-import com.primary_education_system.service.UserService;
+import com.primary_education_system.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.ui.Model;
@@ -11,9 +11,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class UserInfoHeaderController {
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @ModelAttribute()
     public void getCurrentUser(@AuthenticationPrincipal CustomUserDetails currentUser, Model model) {
+        if (currentUser == null) {
+            return;
+        }
+        model.addAttribute("user", userRepository.findOne(currentUser.getUserId()));
+        model.addAttribute("year", currentUser.getYear());
+        model.addAttribute("month", currentUser.getMonth());
+        model.addAttribute("day", currentUser.getDay());
+        model.addAttribute("hour", currentUser.getHour());
+        model.addAttribute("minute", currentUser.getMinutes());
+
     }
 }
