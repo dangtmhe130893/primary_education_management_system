1087d8636998a180507a02d2332b78b8f9beefe2
dangtm <dangtmhe130893@fpt.edu.vn>
2021-11-24 20:09:50 +0700

    update RedirectService

--- src/main/java/com/primary_education_system/service/RedirectService.java
+++ src/main/java/com/primary_education_system/service/RedirectService.java
@@ -1,31 +1,69 @@
 package com.primary_education_system.service;
 
-import com.google.common.base.Objects;
 import com.primary_education_system.config.security.CustomUserDetails;
+import com.primary_education_system.entity.user.RoleEntity;
+import com.primary_education_system.entity.user.UserEntity;
 import com.primary_education_system.enum_type.Roles;
-import org.springframework.security.core.GrantedAuthority;
+import com.primary_education_system.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
+import java.util.Set;
 
 @Service
 public class RedirectService {
 
-    public String getDefaultRedirectUri(CustomUserDetails userDetails) {
-        String uri;
+    @Autowired
+    private UserRepository userRepository;
+
+    public String getDefaultRedirectUri(CustomUserDetails currentUser) {
+        boolean isSystem = false;
+        boolean isStaff = false;
+        boolean isTeacher = false;
+        boolean isAcademicHead = false;
+        boolean isHeadMaster = false;
         boolean isPupilParent = false;
-        Collection<GrantedAuthority> auths = userDetails.getAuthorities();
-        for (GrantedAuthority auth : auths) {
-            if (Objects.equal(auth.getAuthority(), Roles.PUPIL_PARENT.name())) {
+        UserEntity userEntity = userRepository.findOne(currentUser.getUserId());
+        Set<RoleEntity> roles = userEntity.getRoles();
+        for (RoleEntity role : roles) {
+            String roleName = role.getName();
+            if (Roles.SYSTEM_ADMIN.name().equals(roleName)) {
+                isSystem = true;
+            }
+            if (Roles.STAFF.name().equals(roleName)) {
+                isStaff = true;
+            }
+            if (Roles.TEACHER.name().equals(roleName)) {
+                isTeacher = true;
+            }
+            if (Roles.ACADEMIC_HEAD.name().equals(roleName)) {
+                isAcademicHead = true;
+            }
+            if (Roles.HEAD_MASTER.name().equals(roleName)) {
+                isHeadMaster = true;
+            }
+            if (Roles.PUPIL_PARENT.name().equals(roleName)) {
                 isPupilParent = true;
-                break;
             }
         }
-        if (isPupilParent) {
-            uri = "/pupil_parent/";
-        } else {
-            uri = "/admin/account";
+        if (isSystem) {
+            return "/admin/account";
         }
-        return uri;
+        if (isStaff) {
+            return "/admin/tuition_status";
+        }
+        if (isTeacher) {
+            return "/admin/teach_class";
+        }
+        if (isAcademicHead) {
+            return "/admin/time_schedule";
+        }
+        if (isHeadMaster) {
+            return "/admin/tuition";
+        }
+        if (isPupilParent) {
+            return "/home";
+        }
+        return "/home";
     }
 }
