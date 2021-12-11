8a5def321fc2079129de62e81b306a3e2cc14cc5
dangtm <dangtmhe130893@fpt.edu.vn>
2021-12-11 20:28:17 +0700

    add HP pupil

--- src/main/resources/static/custom/js/logout.js
+++ src/main/resources/static/custom/js/logout.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
     $("#logoutLink").click(function (e) {
         e.preventDefault();
-        window.loader.show();
         $.get("/logout", {}, function () {
-            window.loader.hide();
             window.location = "/home";
         });
     });
